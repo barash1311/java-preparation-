c711e772b0e575927f24ae7e09a1473543329259
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-03-16 13:36:31 +0530

    sumn using recusuion

---
+++ src/com/barash/day11/sumn.java
@@ -0,0 +1,13 @@
+package com.barash.day11;
+
+public class sumn {
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+    public static int sum(int n){
+        if(n<=0){
+            return 0;
+        }
+        return n+sum(n-1);
+    }
+}
