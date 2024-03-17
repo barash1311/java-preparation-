4848a72bb9ef270423debca6f3bb9c36a4dd04a2
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-03-17 22:24:19 +0530

    string2

---
+++ src/com/barash/day12/String2.java
@@ -0,0 +1,21 @@
+package com.barash.day12;
+
+public class String2 {
+    public static void main(String[] args) {
+        System.out.println(skip("heaaaaaaaalllllppp"));
+
+    }
+
+
+    static String skip(String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+        char ch = up.charAt(0);
+        if (ch == 'a') {
+            return skip(up.substring(1));
+        } else {
+            return ch + skip(up.substring(1));
+        }
+    }
+}
