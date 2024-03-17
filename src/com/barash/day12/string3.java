3e1ae432a14fa9f1bf317cf926b10fddddcfe67e
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-03-17 22:27:45 +0530

    sunstring 3

---
+++ src/com/barash/day12/string3.java
@@ -0,0 +1,19 @@
+package com.barash.day12;
+
+public class string3 {
+    public static void main(String[] args) {
+        System.out.println(skip("bapplemango"));
+
+    }
+    public static String skip(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+        if(up.startsWith("apple")){
+            return skip(up.substring(5));
+        }
+        else{
+            return up.charAt(0)+skip(up.substring(1));
+        }
+    }
+}
