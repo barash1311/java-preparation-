8f7078d5646a07370815cc5a34c947e4fdead46d
barash sharma <138241217+barash1311@users.noreply.github.com>
2024-03-22 23:31:11 +0530

    update

---
+++ src/com/barash/day14/shape/Main.java
@@ -0,0 +1,15 @@
+package com.barash.day14.shape;
+
+public class Main {
+    public static void main(String[] args) {
+        Shapes s1 = new Shapes();
+        s1.display();
+        Shapes s2=new Square();
+        s2.display();
+//        Square s2 = new Square();
+//        s2.display();
+//        Triangle s3 = new Triangle();
+//        s3.display();
+
+    }
+}
